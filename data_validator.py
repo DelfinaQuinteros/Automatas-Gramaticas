c63cbc650092a735dd44e6eb71e259711ec1af4a
DelfinaQuinteros <65193862+DelfinaQuinteros@users.noreply.github.com>
2022-11-17 21:39:36 -0300

    changess

--- data_validator.py
+++ data_validator.py
@@ -42,26 +42,31 @@ class DataFilter:
                 list_lines.append(line)
         return list_lines
 
-    def get_line_by_conection_id(self, conection_id, lines):
+    @staticmethod
+    def get_line_by_conection_id(conection_id, lines):
         for line in lines:
             if re.findall(conection_id, line):
                 return line
 
-    def get_date(self, line) -> tuple:
+    @staticmethod
+    def get_date(line) -> tuple:
         dates = re.search('((\d{2}\/)+\d{4}) (\d{2}:\d{2})', line)
         try:
             return str(dates.group(0)), str(dates.group(1))
         except:
             return None, None
 
-    def get_seconds(self, line: list) -> int:
+    @staticmethod
+    def get_seconds(line: list) -> int:
         seconds = line[4]
         return int(seconds)
 
+    @staticmethod
     def get_start_date(self, line: list) -> str:
         start_date = line[2]
         return str(start_date)
 
+    @staticmethod
     def get_end_date(self, line: list) -> str:
         end_date = line[3]
         return str(end_date)
